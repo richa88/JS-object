6518de18703243fad7c372b59ec31906df6b9e11
richa88 <91276574+richa88@users.noreply.github.com>
2021-11-26 21:02:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var person1={
     FirstName: "Richa",
     LastName: "Kumari",
     OfficeAddress: "3rd Floor,Lakshmi Complex,Rajendra Nagar,Chennai"
-  }
+  };
 
 console.log("First Person:");
 console.log("Name-"+person1.FirstName+" "+person1.LastName);
@@ -12,7 +12,7 @@ var person2={
     FirstName: "Ekta",
     LastName: "Agrawal",
     OfficeAddress: "1st Floor,Jeevan Deep,Exhibition Road,Banglore"
-}
+};
 
 console.log("Second Person:");
 console.log("Name-"+person2.FirstName+" "+person2.LastName);
@@ -22,7 +22,7 @@ var person3={
   FirstName: "Shivani",
   LastName: "Parmar",
   OfficeAddress: "2nd Floor,Jeevan Prakash,Rajpath Nagar,Delhi"
-}
+};
 
 console.log("Third Person:");
 console.log("Name-"+person3.FirstName+" "+person3.LastName);
